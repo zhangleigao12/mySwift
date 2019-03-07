b6d7c86f0d405e1e06ccb75fb20dbc5e4460b33c
zhangleigao12 <zlggit@163.com>
2019-03-07 09:36:56 +0800

    删除OC，创建Swift

---
+++ SwiftPro/SwiftPro/proClass/page/person/PersonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonViewController.swift
+//  SwiftPro
+//
+//  Created by zhangleigao on 2019/3/7.
+//  Copyright © 2019 zhangleigao. All rights reserved.
+//
+
+import UIKit
+
+class PersonViewController: LGBasicViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
